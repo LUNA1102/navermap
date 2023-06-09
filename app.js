6ffcf98aa45cd4511f5e467f56208eb6b4c16d4f
unknown <moong3u@naver.com>
2023-06-09 21:39:36 +0900

    commit

--- app.js
+++ app.js
@@ -1,9 +1,6 @@
-var map = new naver.maps.Map('map', {
+var mapOptions = {
     center: new naver.maps.LatLng(35.945185, 126.682834),
     zoom: 10
-});
+};
 
-var marker = new naver.maps.Marker({
-    position: new naver.maps.LatLng(35.945185, 126.682834),
-    map: map
-});
+var map = new naver.maps.Map('map', mapOptions);
